ef5149e125ccd09c2e5408582129b003928c8c21
harshbawari <harshbawari@gmail.com>
2021-04-23 14:06:47 +0530

    cert and key

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const hackerRanking = express();
 hackerRanking.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://secure-dawn-75725.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
