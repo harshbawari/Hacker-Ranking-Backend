ab20dc653cb5cc4e79b42db76d04265893c7e54e
harshbawari <harshbawari@gmail.com>
2021-04-23 16:50:55 +0530

    Ranking

--- services/sortLeaders.js
+++ services/sortLeaders.js
@@ -3,11 +3,13 @@ function sort_leaders(hackers) {
     hackers.sort((a, b) => {
         const name_a = a.name.toUpperCase();
         const name_b = b.name.toUpperCase();
+        const percentile_a = (a.competitivePercentile.dataStructures + a.competitivePercentile.algorithms + a.competitivePercentile.cpp + a.competitivePercentile.java + a.competitivePercentile.python + a.competitivePercentile.html + a.competitivePercentile.javascript) / 7;
+        const percentile_b = (b.competitivePercentile.dataStructures + b.competitivePercentile.algorithms + b.competitivePercentile.cpp + b.competitivePercentile.java + b.competitivePercentile.python + b.competitivePercentile.html + b.competitivePercentile.javascript) / 7;
 
-        if(name_a < name_b) {
+        if(percentile_a > percentile_b) {
             return -1;
         }
-        if(name_a > name_b) {
+        if(percentile_a < percentile_b) {
             return 1;
         }
 
@@ -16,7 +18,7 @@ function sort_leaders(hackers) {
 
     console.log('hackers: ', hackers);
 
-    return hackers.slice(0, 2);
+    return hackers.slice(0, 3);
 }
 
 exports.sort_leaders = sort_leaders;
