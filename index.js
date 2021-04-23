53d44725c8953e65e8ae141252c2caf631f75d7d
harshbawari <harshbawari@gmail.com>
2021-04-23 13:40:46 +0530

    localhost

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const hackerRanking = express();
 hackerRanking.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://103.253.173.27:3000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
